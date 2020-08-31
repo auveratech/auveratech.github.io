592936acdc22d96b3d1ee04bed1a34e5747a27ee
Bradly Knox <knoxba@hotmail.com>
2020-08-31 04:07:37 -0700

    responsiveness update, revamped Landing, mobile friendly index

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
-    console.log("loaded.");
+    $('div').on('mouseover', function () {
+        $(this).toggleClass('show-description');
+    });
+    $('div').on('mouseout', function () {
+        $(this).toggleClass('show-description');
+    });
 });
 
