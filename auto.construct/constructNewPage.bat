b3f23dc90fa5411dfac220dccc3cac2ecc1299b1
Bradly Knox <knoxba@hotmail.com>
2020-09-01 10:39:40 -0700

    responsiveness update, revamped Landing, mobile friendly index

--- auto.construct/constructNewPage.bat
+++ auto.construct/constructNewPage.bat
@@ -11,7 +11,7 @@ type nul > standard-%newPage%.js
 type nul > jqery-%newPage%.js
 cd ..
 cd auto.render
-echo sass C:\Users\knoxb\Desktop\github-page\scss\%newPage%-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-%newPage%.css > compile-%newPage%.bat
+echo sass C:\Users\knoxb\Desktop\github-page\scss\%newPage%-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-build-%newPage%.css > compile-%newPage%.bat
 echo call compile-%newPage%.bat >> #EXECUTE_DEPLOY_SITE.bat
 
 
--- auto.render/compile-partners.bat
+++ auto.render/compile-partners.bat
@@ -1,1 +1,1 @@
-sass C:\Users\knoxb\Desktop\github-page\scss\partners-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-partners.css 
+sass C:\Users\knoxb\Desktop\github-page\scss\partners-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-build-partners.css 
