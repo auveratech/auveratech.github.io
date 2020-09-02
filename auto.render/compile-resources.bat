aaff3c41375351da1b1443cb66961e7a3228be3b
Bradly Knox <knoxba@hotmail.com>
2020-09-02 09:18:44 -0700

    constructed new page added external resources

--- auto.render/compile-resources.bat
+++ auto.render/compile-resources.bat
@@ -1,1 +1,1 @@
-sass C:\Users\knoxb\Desktop\github-page\scss\resources-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-resources.css 
+sass C:\Users\knoxb\Desktop\github-page\scss\resources-styles.scss C:\Users\knoxb\Desktop\github-page\build\current-build-resources.css 
